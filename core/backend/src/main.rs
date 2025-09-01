451d3cad87d1e34cdf6f378dd1bc458bc99bea78
Esteve Autet Alexe <esteve@memw.es>
2025-09-01 19:19:23 +0200

    chore(setup): Base project setup (#3).
    * chore(setup): Create frontend project with yew boostrap.
    
    * chore(setup): Create backend project.

---
+++ core/backend/src/main.rs
@@ -0,0 +1,23 @@
+use std::io::Error as IoError;
+
+use actix_web::{main, web::get, App, HttpResponse, HttpServer};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum AppError {
+    #[error("Error starting the server.")]
+    Server(#[from] IoError)
+}
+
+#[main]
+async fn main() -> Result<(), AppError> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", get().to(async || HttpResponse::Ok().body("Hello World!")))
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await?;
+
+    Ok(())
+}

34988e43f709c53b7cef4f9edafc3cc4fec1356c
stifskere <esteve@memw.es>
2025-09-04 21:49:10 +0200

    chore(setup): Add testing suite along Justfile.

--- core/backend/src/main.rs
+++ core/backend/src/main.rs
@@ -1,23 +1,30 @@
+#![deny(clippy::pedantic)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::match_like_matches_macro)]
+
 use std::io::Error as IoError;
 
-use actix_web::{main, web::get, App, HttpResponse, HttpServer};
+use actix_web::web::get;
+use actix_web::{App, HttpResponse, HttpServer, main};
 use thiserror::Error;
+
+#[cfg(test)]
+mod tests;
 
 #[derive(Error, Debug)]
 enum AppError {
     #[error("Error starting the server.")]
-    Server(#[from] IoError)
+    Server(#[from] IoError),
 }
 
 #[main]
 async fn main() -> Result<(), AppError> {
     HttpServer::new(|| {
-        App::new()
-            .route("/", get().to(async || HttpResponse::Ok().body("Hello World!")))
+        App::new().route("/", get().to(async || HttpResponse::Ok().body("Hello World!")))
     })
-        .bind(("0.0.0.0", 8081))?
-        .run()
-        .await?;
+    .bind(("0.0.0.0", 8081))?
+    .run()
+    .await?;
 
     Ok(())
 }

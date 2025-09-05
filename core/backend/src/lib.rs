78385d66db01930164a9ba839cf5366c9a5067b3
stifskere <esteve@memw.es>
2025-09-06 00:14:11 +0200

    chore(tests): Finish testing setup.

---
+++ core/backend/src/lib.rs
@@ -0,0 +1,6 @@
+
+#[cfg(test)]
+mod tests;
+
+// Export routes for tests.
+pub mod routes;
--- core/frontend/src/components/app.rs
+++ core/frontend/src/components/app.rs
@@ -3,8 +3,6 @@ use yew::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main>
-            <h1>{ "Hello World!" }</h1>
-        </main>
+        <h1>{ "Hello World!" }</h1>
     }
 }

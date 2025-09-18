9e9aaa7f1417eca6e84a98682655fe5fac38fc1b
stifskere <esteve@memw.es>
2025-09-18 03:09:35 +0200

    refactor(notifications): Refactor builders for all components.

---
+++ core/frontend/src/utils/notifications/notification.rs
@@ -0,0 +1,104 @@
+use bon::Builder;
+use instant::{Duration, Instant};
+use palette::Srgb;
+use yew::Properties;
+
+use crate::utils::colors::{ERROR_RED, INFO_BLUE, SUCCESS_GREEN};
+use crate::utils::notifications::component::{
+    ActionButton,
+    DropDown,
+    NotificationComponent,
+    RedirectButton,
+};
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum NotificationLevel {
+    Info,
+    Success,
+    Error,
+}
+
+#[derive(Builder, Clone, PartialEq, Debug)]
+pub struct Notification<'c> {
+    #[builder(field)]
+    components: Vec<NotificationComponent<'c>>,
+
+    title: String,
+    message: String,
+    #[builder(default = NotificationLevel::Info)]
+    level: NotificationLevel,
+
+    #[builder(default = Instant::now())]
+    created_at: Instant,
+    #[builder(default = Duration::from_secs(5))]
+    duration: Duration,
+}
+
+impl<'c> Notification<'c> {
+    #[inline]
+    pub fn components(&'c self) -> &'c [NotificationComponent<'c>] {
+        &self.components
+    }
+
+    #[inline]
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    #[inline]
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+
+    #[inline]
+    pub fn level(&self) -> NotificationLevel {
+        self.level
+    }
+
+    #[inline]
+    pub fn created_at(&self) -> Instant {
+        self.created_at
+    }
+
+    #[inline]
+    pub fn duration(&self) -> Duration {
+        self.duration
+    }
+
+    pub fn is_expired(&self) -> bool {
+        self.created_at()
+            .elapsed()
+            > self.duration()
+    }
+}
+
+impl NotificationLevel {
+    #[inline]
+    pub const fn to_color(&self) -> Srgb<u8> {
+        match self {
+            NotificationLevel::Info => INFO_BLUE,
+            NotificationLevel::Success => SUCCESS_GREEN,
+            NotificationLevel::Error => ERROR_RED,
+        }
+    }
+}
+
+impl<'c, S: notification_builder::State> NotificationBuilder<'c, S> {
+    pub fn add_redirect_button(mut self, button: RedirectButton) -> Self {
+        self.components
+            .push(button.into());
+        self
+    }
+
+    pub fn add_action_button(mut self, button: ActionButton<'c>) -> Self {
+        self.components
+            .push(button.into());
+        self
+    }
+
+    pub fn add_dropdown(mut self, dropdown: DropDown<'c>) -> Self {
+        self.components
+            .push(dropdown.into());
+        self
+    }
+}
